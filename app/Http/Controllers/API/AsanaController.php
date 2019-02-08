412810c21f2bf9e1feeba499906219ea05db9a5e
norbertglen <liddypedia@gmail.com>
2019-02-08 11:53:24 +0300

    update controller and route

--- app/Http/Controllers/API/AsanaController.php
+++ app/Http/Controllers/API/AsanaController.php
@@ -1,35 +1,37 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Twilio\Rest\Client;
 
-class AutomateController extends Controller {
-	/**
-	 * send notifications
-	 */
-	public function notifications() {
-		$data = json_decode( file_get_contents( 'php://input' ), true );
+class AsanaController extends Controller {
+
+	public function notifications( Request $request ) {
 		$message = "";
 		// create message
-		if ( isset( $data['task_id'] ) ) {
+		if ( $request->task_id ) {
 			$message = "A new task has been created.";
-		} elseif ( isset( $data['created_by'] ) ) {
-			$message = $data['created_by'] . " has commented on " . $data['task_name'].".";
-		} elseif ( isset( $data['task_completed'] ) ) {
-			$message = "Task " . $data['task_name'] . " has been completed.";
+		} else if ( $request->created_by ) {
+			$message = $request->created_by . " has commented on " . $request->task_name . ".";
+		} else if ( $request->task_completed ) {
+			$message = "Task " . $request->task_name . " has been completed.";
 		}
+
 		// Your Account SID and Auth Token from twilio.com/console
 		$account_sid = getenv( 'ACCOUNT_SID' );
 		$auth_token  = getenv( 'TWILIO_TOKEN' );
+
 		// A Twilio number you own with SMS capabilities
 		$twilio_number = getenv( 'TWILIO_NUMBER' );
+
 		// List of project members phone numbers
-		$numbers = [ 'list of member phone number' ];
+		$numbers = [ 'list of member phone numbers' ];
 		$client  = new Client( $account_sid, $auth_token );
+
 		//loop through the numbers
 		foreach ( $numbers as $number ) {
-//			dd( $number );
 			$client->messages->create(
 			// Where to send a text message (your cell phone?)
 				$number,
@@ -39,6 +41,8 @@ class AutomateController extends Controller {
 				)
 			);
 		}
+
 		return "ok";
 	}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::post( '/createtask', 'AsanaController@index' );
-Route::post( '/notifications', 'AutomateController@notifications' );
